8e756c4f245bc88c847afafeae8ab728a66938c7
Ryan Wendell <ryan@blubracket.com>
2022-02-15 16:47:55 -0600

    fix: use login logic

--- components/authenticate/Authenticate.tsx
+++ components/authenticate/Authenticate.tsx
@@ -2,33 +2,41 @@ import { Web3Provider } from "@ethersproject/providers";
 import { ethers } from "ethers";
 import { Button } from "grommet";
 import Link from "next/link";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 
 
 
 export const Authenticate = () => {
-  const [provider, setProvider] = useState<Web3Provider|null> (null);
+  const [provider, setProvider] = useState<Web3Provider | null>(null);
+  const [login, setLogin] = useState(false)
 
+
+  // TODO This is very VERY ugly.  Research a better implementation of how to take care of this
   useEffect(
-    () => setProvider(new ethers.providers.Web3Provider(window.ethereum)),
-    []
+    () => {
+      setProvider(new ethers.providers.Web3Provider(window.ethereum))
+      const isLoggedIn = async () => setLogin(!!(await provider!?.listAccounts())?.length)
+      isLoggedIn();
+    },
+    [provider, login, setProvider]
   );
 
-  async function getAccount() {
+  const getAccount = async () => {
     await provider!.send("eth_requestAccounts", []);
   }
 
+
   return (
     <>
-      {provider?.listAccounts() ? (
+      {login ? (
         <Link href={"/search"} passHref>
-        <Button primary label="Find Tables" />
+          <Button primary label="Find Tables" />
         </Link>
       ) : (
         <>
           <h2>Please authenticate with MetaMask 🦊</h2>
-          <button onClick={() => getAccount()}>Authenticate</button>
+          <Button secondary onClick={() => getAccount()} label="Authenticate" />
         </>
       )}
     </>
