cc7893edabc87fbd5bb6286885fd0db209f68667
Ryan Wendell <ryan@blubracket.com>
2022-02-15 19:33:19 -0600

    clean: cleanup boilerplate

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     
 
     return(
-        <h1>Welcome to Pok3r, proximity based poker DApp 🃏</h1>
+        <h1>Welcome to Pok3r, the proximity based poker DApp 🃏</h1>
     )
 }
 
